725acae80116fd11b97128dc8471d1f419193a7e
Sideswipe5 <152334576+Sideswipe5@users.noreply.github.com>
2025-02-18 11:36:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import { AppRegistry } from 'react-native-web';  // For web compatibility
 import App from './App.web.js';
 
+alert("TEST");
+
 // Register the app to run on the web
 AppRegistry.registerComponent('main', () => App);
+
+alert("TEST2");
 
 // Run the application
 AppRegistry.runApplication('main', {
   initialProps: {},  // You can pass initial props if needed
   rootTag: document.getElementById('root'),  // This ensures the app is attached to the DOM
 });
+
+alert("TEST3");
